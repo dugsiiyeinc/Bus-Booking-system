8669fd5a9b5fcbe4afde97a472c65eedecefe229
abdalegurey <[200~abdalegureyguled@gmail.com~>
2025-04-21 10:35:12 +0300

    feat: Implement CRUD operations for Bus entity
    - Created Bus index and create pages
    - Integrated Supabase to handle data fetch, insert, and update
    - Designed UI using Tailwind CSS
    - Added form validation with error states
    - Enabled dynamic form behavior for both create and edit modes

---
+++ src/Components/SidebarDashboard.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { Home, Users, Settings, Menu, X, Sun, Moon, LogOut } from 'lucide-react';
+import { Bus } from 'lucide-react';
+
+
+const SidebarDashboard = () => {
+  return (
+      <aside className="hidden md:flex md:flex-col w-64 bg-white dark:bg-gray-800 shadow-lg p-6">
+      <h2 className="text-2xl font-extrabold text-blue-600 dark:text-blue-400 mb-8">Admin Panel</h2>
+      <nav className="flex-1 space-y-4">
+        <NavLink
+          to="/Dashboard/overview"
+          className={({ isActive }) =>
+            `flex items-center gap-3 px-4 py-2 rounded-lg transition font-medium ${
+              isActive
+                ? 'bg-blue-100 text-blue-700 dark:bg-blue-900 dark:text-blue-300 shadow-inner'
+                : 'hover:bg-blue-50 hover:text-blue-600 dark:hover:bg-gray-700'
+            }`
+          }
+        >
+          <Home size={20} />
+          Overview
+        </NavLink>
+    
+       
+    
+        <NavLink
+          to="/Dashboard/BusesIndex"
+          className={({ isActive }) =>
+            `flex items-center gap-3 px-4 py-2 rounded-lg transition font-medium ${
+              isActive
+                ? 'bg-blue-100 text-blue-700 dark:bg-blue-900 dark:text-blue-300 shadow-inner'
+                : 'hover:bg-blue-50 hover:text-blue-600 dark:hover:bg-gray-700'
+            }`
+          }
+        >
+          <Bus size={20} />
+
+        Buses
+        </NavLink>
+
+        <NavLink
+          to="/Dashboard/users"
+          className={({ isActive }) =>
+            `flex items-center gap-3 px-4 py-2 rounded-lg transition font-medium ${
+              isActive
+                ? 'bg-blue-100 text-blue-700 dark:bg-blue-900 dark:text-blue-300 shadow-inner'
+                : 'hover:bg-blue-50 hover:text-blue-600 dark:hover:bg-gray-700'
+            }`
+          }
+        >
+          <Users size={20} />
+          Users
+        </NavLink>
+      </nav>
+    </aside>
+  )
+}
+
+export default SidebarDashboard
